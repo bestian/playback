8ca6b83b53d9de98956375d1a7c12ee777688741
Bestian Tang <bestian@gmail.com>
2024-08-26 18:28:03 +0800

    u

--- src/data/announce.js
+++ src/data/announce.js
@@ -3,9 +3,9 @@ export const announces = [
     //    text: '九月二階工作坊【心領神會】09/28, 09/29籌備中：有興趣的朋友請先空出09/28, 09/29上午時段',
         // h: 'https://forms.gle/dCEW3m3a1pJfj5kP7'
     //},
-    {date: '2024-08-14', 
-        text: '九月初階工作坊【從心出發】09/14, 09/15開放報名中(8/26截止)',
-        h: 'https://forms.gle/yquAWnrjzB1nuvys9'}
+    //{date: '2024-08-14', 
+    //    text: '九月初階工作坊【從心出發】09/14, 09/15開放報名中(8/26截止)',
+    //    h: 'https://forms.gle/yquAWnrjzB1nuvys9'}
 //  {date: '2024-07-31', text: '八月工作坊已確認成行，延長報名至08/06', h: 'https://forms.gle/6yQVzVvWew6vQ1eH8'},
 //  {date: '2024-07-18', text: '八月工作坊籌備中：有興趣的朋友請先空出08/08, 08/22上午時段'},
 
