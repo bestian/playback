c5d2d33217351d1e630c6e6facf8312cfec8c18f
Bestian Tang <bestian@gmail.com>
2025-03-03 06:17:40 +0800

    Create Lev3_template.vue

---
+++ src/courses/Lev3_template.vue
@@ -0,0 +1,42 @@
+<template lang="pug">
+.courses
+  h1.ui.header Playback二階體驗工作坊
+    .sub.header 四種基本元素、五個基本形式
+  .ui.segmant.container
+    iframe(width="100%", 
+      height="600px",
+      frameborder="0",
+      allowfullscreen,
+      :src="'https://docs.google.com/document/d/' + YourDocumentID + '/preview'")
+
+</template>
+
+<script>
+
+export default {
+  name: 'Lev2Course',
+  data () {
+    return {
+      YourDocumentID: '1xGUO865Tq_9tkYLGEjcZp7dXkAPMvRMH65-kGKbAmcs'
+    }
+  }
+}
+</script>
+
+<style scoped>
+.ui.card {
+  padding: 1em;
+}
+
+img.card-img {
+  max-width: 33vmin;
+}
+
+p {
+  font-size: 18px;
+  text-align: left;
+  max-width: 620px;
+  margin: .6em auto;
+  color: rgb(64, 3, 206);
+}
+</style>
